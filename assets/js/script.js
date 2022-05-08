277223642e260efa5c22e3178cd415600f796ce3
Chris <chrismanfredijr@gmail.com>
2022-05-08 18:43:01 -0400

    added modal and get more results modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,43 @@
 var fmKey = "2097d3a5f8da51f146d0e4e47efde651";
-var dbKey = "2";
-var artistName = "all them witches"
-var limit = "5";
-
-
+var resultNumber = 0; 
+var limit = 20;
+var artistName;
+var youtubeKey = "AIzaSyAruDAmqTVC79gNs-7-sHyVx1zyaRzYPis";
+var youtubeKey2 = "AIzaSyCv3abDl7RNfGiICt80KVPOx22JWsDSBro";
 
 $("#searchArtist").click(function (event) {
     event.preventDefault();
-    var artist = $("#input").val();
-    if (artist !== "") {
-        getRecs(artist)
+    artistName = $("#input").val();
+    resultNumber = 0;
+    if (artistName !== "") {
+        getRecs(artistName,resultNumber)
     }
 
 })
 
-var getRecs = function (artistName) {
-    var lastFmAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=1&artist=" + artistName + "&api_key=" + fmKey + "&format=json";
+var getRecs = function (artistName,resultNumber) {
+    var lastFmAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit="+ limit +"&artist=" + artistName + "&api_key=" + fmKey + "&format=json";
+    
     fetch(lastFmAPI).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                var artistNameRec = data.similarartists.artist[0].name;
-                console.log(artistNameRec);
-
+                var artistNameRec = data.similarartists.artist[resultNumber].name;
                 var lastFmRecAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistNameRec + "&api_key=" + fmKey + "&format=json";
+            
                 fetch(lastFmRecAPI).then(function (response) {
                     if (response.ok) {
                         response.json().then(function (data) {
+                           
                             var artistBioLong = data.artist.bio.content;
                             var artistBioArray = artistBioLong.split(".");
-                            var artistBio = artistBioArray[0] + "." + artistBioArray[1] + "." + artistBioArray[2];
-                            var youtubeAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAruDAmqTVC79gNs-7-sHyVx1zyaRzYPis&type=video&maxResults=1&q=" + artistNameRec
+                            var artistBio = artistBioArray[0] + "." + artistBioArray[1] + "." + artistBioArray[2] + "." + artistBioArray[3] + ".";
+                            var youtubeAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + youtubeKey2 + "&type=video&maxResults=1&q=" + artistNameRec
+                            
                             fetch(youtubeAPI).then(function (response) {
                                 if (response.ok) {
                                     response.json().then(function (data) {
                                         videoId = data.items[0].id.videoId;
-                                        buildRecs(artistNameRec, artistBio, artistName, videoId);
+                                        buildRecs(artistNameRec, artistBio, artistName, videoId, artistBioLong);
                                     });
                                 };
                             })
@@ -49,28 +52,35 @@ var getRecs = function (artistName) {
 
 };
 
+var buildRecs = function (artistNameRec, artistBio, artistName, videoId, artistBioLong) {
 
+    $("#recs").css("display", "block");
+    $("#intro-recs").html("if you like " + artistName + " then you may enjoy....");
+    $("#videoPlayer").attr("src", "https://www.youtube.com/embed/" + videoId);
+    $("#recName").html(artistNameRec);
+    $("#recBio").html(artistBio);
 
-var buildRecs = function (artistNameRec, artistBio, artistName, videoId) {
-
-    $("#recs").html("");
-    var recBlock = "<div class =\"title is-3 has-text-centered\" > if you like " + artistName + " than you may enjoy....</div>" +
-        "<div class = \"columns is-centered mb-10\">" +
-        "<div class =\"column is-two-fifths\">" +
-        "<figure class=\"image is-16by9\">" +
-        "<iframe class=\"has-ratio\" width=\"360\" height=\"270\" src=\"https://www.youtube.com/embed/"+ videoId +"\" frameborder=\"0\" allowfullscreen></iframe> " +
-        "</figure>" +
-        "</div>" +
-        "<div class = \"column is-two-fifths\">" +
-        "<h1 class = \"title is-3\">" + artistNameRec + "</h1>" +
-        "<p class = \"content is-medium\">" + artistBio + "</p>" +
-        "<button class = \"button is-info\">learn more</button>" +
-        "</div>" +
-        "</div>";
-    $("#recs").append(recBlock);
+    $("#modal-artist").html(artistNameRec);
+    $("#long-bio").html(artistBioLong);
 
 };
 
 
+//expand modal
+$("#learn-more").click(function() {
+    $("#modal-more-info").addClass("is-active");
+});
 
+//close modal
+$(".delete").click(function() {
+    $("#modal-more-info").removeClass("is-active")
+});
 
+$("#more-recs").click(function() {
+    if(resultNumber === limit){
+        resultNumber = 0;
+    }else{
+        resultNumber++;
+    };
+    getRecs(artistName, resultNumber);
+});
