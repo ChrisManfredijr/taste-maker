e334830f027f751fae54f3b354977cec72f0f88a
Chris <chrismanfredijr@gmail.com>
2022-05-09 19:26:04 -0400

    intial optimizations

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# taste-maker
+# Tastemakr
+
+![tastemakr webiste](./assets/images/website.PNG)
+
+## Link 
+https://chrismanfredijr.github.io/taste-maker/
+
+## Purpose
+    to build a website that allows users to input the name of an artist and get a recommendation based on their input. Users can also click to see more results that would match their preferences. Each recommendation is accompanied by a youtube player of their music and a short bio about them. Users can expand the bio to learn more 
+
+## Built with 
+#### Languages:
+- HTML
+- CSS
+- JavaScript 
+
+#### third party APIs:
+- Jquery 
+- Jquery UI
+- Bulma 
+
+#### Web APIs:
+- last.fm API
+- YouTube Data API
+
+### Built by:
+Chris Manfredi, Ekow Andoh, Dallas Bailey, Benjamin Lawton
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,69 @@
 var fmKey = "2097d3a5f8da51f146d0e4e47efde651";
-var resultNumber = 0; 
+var resultNumber = 0;
 var limit = 20;
 var artistName;
-var youtubeKey = "AIzaSyAruDAmqTVC79gNs-7-sHyVx1zyaRzYPis";
-var youtubeKey2 = "AIzaSyCv3abDl7RNfGiICt80KVPOx22JWsDSBro";
+var youtubeKey = ["AIzaSyAruDAmqTVC79gNs-7-sHyVx1zyaRzYPis", "AIzaSyCv3abDl7RNfGiICt80KVPOx22JWsDSBro", "AIzaSyBko84VIeCaWrdK8EspDLkDCkuFYDZsQAU", "AIzaSyDCUTgXNuEadfA_AUekjgy8ERv1WY5AT4o", "AIzaSyCbmbvdEaEQ0-On5-s4Ewb6B33BzsGt6oU"]
 
-$("#searchArtist").click(function (event) {
+
+
+$("#search").submit(function(event) {
     event.preventDefault();
     artistName = $("#input").val();
     resultNumber = 0;
     if (artistName !== "") {
-        getRecs(artistName,resultNumber)
+        getRecs(artistName, resultNumber)
+    }
+});
+
+$("#searchArtist").click(function(event){
+    event.preventDefault();
+    artistName = $("#input").val();
+    resultNumber = 0;
+    if (artistName !== "") {
+        getRecs(artistName, resultNumber)
     }
 
 })
 
-var getRecs = function (artistName,resultNumber) {
-    var lastFmAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit="+ limit +"&artist=" + artistName + "&api_key=" + fmKey + "&format=json";
-    
+
+var getRecs = function (artistName, resultNumber) {
+    var lastFmAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=" + limit + "&artist=" + artistName + "&api_key=" + fmKey + "&format=json";
+
     fetch(lastFmAPI).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
+           
+                
                 var artistNameRec = data.similarartists.artist[resultNumber].name;
                 var lastFmRecAPI = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artistNameRec + "&api_key=" + fmKey + "&format=json";
-            
+
                 fetch(lastFmRecAPI).then(function (response) {
                     if (response.ok) {
                         response.json().then(function (data) {
-                           
+
                             var artistBioLong = data.artist.bio.content;
                             var artistBioArray = artistBioLong.split(".");
                             var artistBio = artistBioArray[0] + "." + artistBioArray[1] + "." + artistBioArray[2] + "." + artistBioArray[3] + ".";
-                            var youtubeAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + youtubeKey2 + "&type=video&maxResults=1&q=" + artistNameRec
                             
-                            fetch(youtubeAPI).then(function (response) {
-                                if (response.ok) {
-                                    response.json().then(function (data) {
-                                        videoId = data.items[0].id.videoId;
-                                        buildRecs(artistNameRec, artistBio, artistName, videoId, artistBioLong);
-                                    });
-                                };
-                            })
-                            
+                            var i = 0;
+                            var keyChecker = function() {
+                                var youtubeAPI = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + youtubeKey[i] + "&type=video&maxResults=1&q=" + artistNameRec + "music video";
+                                fetch(youtubeAPI).then(function (response) {
+                                    if (response.ok) {
+                                        response.json().then(function (data) {
+                                            videoId = data.items[0].id.videoId;
+                                            buildRecs(artistNameRec, artistBio, artistName, videoId, artistBioLong);
+                                            
+                                        });
+                                    }else{
+                                        i++;
+                                        keyChecker();
+                                    };
+                                })
+                    
+                            }
+
+                            keyChecker();
                         });
                     };
                 });
@@ -55,7 +77,8 @@ var getRecs = function (artistName,resultNumber) {
 var buildRecs = function (artistNameRec, artistBio, artistName, videoId, artistBioLong) {
 
     $("#recs").css("display", "block");
-    $("#intro-recs").html("if you like " + artistName + " then you may enjoy....");
+    var originalArtist = artistName.italics();
+    $("#intro-recs").html("If you like " + originalArtist + " then you may enjoy....");
     $("#videoPlayer").attr("src", "https://www.youtube.com/embed/" + videoId);
     $("#recName").html(artistNameRec);
     $("#recBio").html(artistBio);
@@ -67,20 +90,37 @@ var buildRecs = function (artistNameRec, artistBio, artistName, videoId, artistB
 
 
 //expand modal
-$("#learn-more").click(function() {
+$("#learn-more").click(function () {
     $("#modal-more-info").addClass("is-active");
 });
 
 //close modal
-$(".delete").click(function() {
+$(".delete").click(function () {
     $("#modal-more-info").removeClass("is-active")
 });
 
-$("#more-recs").click(function() {
-    if(resultNumber === limit){
+$("#more-recs").click(function () {
+    if (resultNumber === limit) {
         resultNumber = 0;
-    }else{
+    } else {
         resultNumber++;
     };
     getRecs(artistName, resultNumber);
 });
+
+//auto-complete
+$(function () {
+    $("#input").autocomplete({
+        source: function(request, response) {
+            var results = $.ui.autocomplete.filter(artistList, request.term);
+            response(results.slice(0, 10));
+        }
+    },
+        {
+            delay: 200,
+            minLength: 3,
+            autoFocus: true,
+           
+        });
+});
+
